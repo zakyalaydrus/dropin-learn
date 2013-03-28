423f05578ced9489b02f8c9aab73e91b862f70cd
yansyaf <yansyaf@gmail.com>
2013-03-28 08:07:18 +0000

    initial design

---
+++ application/views/pages/home.php
@@ -0,0 +1,276 @@
+<div id="home-container">
+<div id="home-video">
+	
+<video controls="controls"  poster="video/Business-Model-Canvas-Explained.jpg" width="360" height="203" title="Business-Model-Canvas-Explained">
+<source src="video/Business-Model-Canvas-Explained.m4v" type="video/mp4" />
+<source src="video/Business-Model-Canvas-Explained.webm" type="video/webm" />
+<source src="video/Business-Model-Canvas-Explained.ogv" type="video/ogg" />
+<source src="video/Business-Model-Canvas-Explained.mp4" />
+</object>
+</video>
+	
+</div>
+<div id="home-list-container">
+	<div>
+		<img src="<?php echo base_url(); ?>assets/images/home_dropin_logo.png" />
+	</div>
+	<div id="home-list">
+		<ul>
+			<li>THE SIMPLEST WAY TO ORGANIZED YOUR LEARNING GROUP</li>
+			<li>THE RIGHT PLACE TO DISCUSS YOUR ACADEMIC PROBLEM</li>
+			<li>THE BEST PLACE TO SHARE AND LEARN EVERYTHING</li>
+			<li>AND IT'S FREE</li>
+		</ul>
+	</div>
+	<div id="home-list-learnmore">
+	<a href="<?php echo site_url('about') ?>">Learn more</a> &nbsp; <img src="<?php echo base_url(); ?>assets/images/next.png"/>
+	</div>
+</div>
+</div>
+
+<div style="height: 30px;"></div>
+
+<div id="category-option">
+<div class="radio-toolbar">  
+<form action="/html/codes/html_form_handler.cfm" method="get">
+    <input type="radio" id="radio_professional" name="radios" value="professional">
+    <label for="radio_professional">PROFESSIONAL</label>
+
+    <input type="radio" id="radio_kuliah" name="radios" value="kuliah">
+    <label for="radio_kuliah">KULIAH</label>
+
+    <input type="radio" id="radio_sma" name="radios" value="sma">
+    <label for="radio_sma">SMA/SMK</label>
+
+    <input type="radio" id="radio_smp" name="radios" value="smp">
+    <label for="radio_smp">SMP</label>
+
+    <input type="radio" id="radio_sd" name="radios" value="sd">
+    <label for="radio_sd">SD</label>
+
+    <input type="radio" id="radio_hobby" name="radios" value="hobby">
+    <label for="radio_hobby">HOBBY</label>
+
+    <input type="radio" id="radio_other" name="radios" value="others">
+    <label for="radio_other">OTHERS</label>
+</form>	
+</div>
+</div>	
+
+<div style="height: 60px;">
+
+	<div id="subcategory-initial">
+		&nbsp;&nbsp;&nbsp;&nbsp;Choose category above and start learn something new!
+	</div>
+	
+	<div id="subcategory-professional" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('about') ?>">SAR</a>
+		<a href="<?php echo site_url('products') ?>">VERILOG</a>
+	</div>	
+	</div>
+
+	<div id="subcategory-kuliah" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('category') ?>">ELEKTRONIKA</a>
+		<a href="<?php echo site_url('category') ?>">KOMPUTER</a>
+		<a href="<?php echo site_url('category') ?>">INFORMATIKA</a>
+		<a href="<?php echo site_url('category') ?>">FARMASI</a>
+	</div>	
+	</div>	
+
+	<div id="subcategory-sma" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('category') ?>">MATEMATIKA</a>
+		<a href="<?php echo site_url('category') ?>">FISIKA</a>
+		<a href="<?php echo site_url('category') ?>">KIMIA</a>
+		<a href="<?php echo site_url('category') ?>">BIOLOGI</a>
+		<a href="<?php echo site_url('category') ?>">SEJARAH</a>
+		<a href="<?php echo site_url('category') ?>">SOSIOLOGI</a>
+	</div>	
+	</div>	
+
+	<div id="subcategory-smp" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('category') ?>">MATEMATIKA</a>
+		<a href="<?php echo site_url('category') ?>">FISIKA</a>
+		<a href="<?php echo site_url('category') ?>">KIMIA</a>
+		<a href="<?php echo site_url('category') ?>">BIOLOGI</a>
+		<a href="<?php echo site_url('category') ?>">SEJARAH</a>
+	</div>	
+	</div>	
+
+	<div id="subcategory-sd" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('category') ?>">MATEMATIKA</a>
+		<a href="<?php echo site_url('category') ?>">IPS</a>
+		<a href="<?php echo site_url('category') ?>">IPA</a>
+	</div>	
+	</div>	
+	
+	<div id="subcategory-hobby" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('category') ?>">GARDENING</a>
+	</div>	
+	</div>	
+
+	<div id="subcategory-other" class="hide">
+	<div class="label-toolbar">  
+		<a href="<?php echo site_url('category') ?>">UNKNOWN</a>
+	</div>	
+	</div>	
+	
+</div>
+
+<div style="height: 30px;"></div>
+
+<div id="user-option">
+<form action="/html/codes/html_form_handler.cfm" method="get">
+<div class="radio-toolbar">  
+    <input type="radio" id="radio1-user" name="radios-subcategory" value="all" checked>
+    <label for="radio1-user">NEWS FEED</label>
+
+    <input type="radio" id="radio2-user" name="radios-subcategory" value="topdropper">
+    <label for="radio2-user">TOP DROPPER</label>
+</div>
+</form>
+</div>
+
+<div id="user-box">
+	<div id="user-box-content">
+	
+		<!-- FIRST CONTENT -->
+		<div id="user-box-content-container">
+
+		<div id="user-box-content-icon">
+			<img src="<?php echo base_url(); ?>assets/images/user1.png" />
+		</div>
+		<div id="user-box-content-text">		
+		<font style="font-family: Gisha; font-size: 0.9em; color: rgb(59,89,152)"><strong>
+TA Topik Mikroelektronika</strong>
+		<font style="font-family: Gisha; font-size: 0.9em; color: rgb(131,130,129)">
+- 1 November 2012
+		</font>
+		<p style="font-family: Gisha; font-size: 0.9em; color: black;"> 
+Examine the history of MIT through the lens of science and technology. Beginning with its founding in 1861, the lecture videos cover topics like William Barton Rogers,educational philosophy, and the Institute’s relationship with Boston. 
+		</p>
+		<p style="font-family: Gisha; font-size: 0.8em; color: rgb(59,89,152);"> 
+Comment 120
+		</p>
+		</div>
+		
+		</div>
+		
+		<!-- SECOND CONTENT -->
+		<div id="user-box-content-container" style="background-color: rgb(222,222,221)">
+
+		<div id="user-box-content-icon">
+			<img src="<?php echo base_url(); ?>assets/images/user2.png" />
+		</div>
+		<div id="user-box-content-text">		
+		<font style="font-family: Gisha; font-size: 1em; color: rgb(59,89,152)"><strong>
+Materi Kuliah Manfar</strong>
+		<font style="font-family: Gisha; font-size: 0.9em; color: rgb(131,130,129)">
+- 1 November 2012
+		</font>
+		<p style="font-family: Gisha; font-size: 0.9em; color: black;"> 
+Whether you're in college, graduate school, high school or taking some night classes after work, there are some important ways to stay organized to ensure success in school. And if you're finding that schoolwork is taking up all of your time, MainStreet has some suggestions on how to balance schoolwork with everything else. 
+		</p>
+		<p style="font-family: Gisha; font-size: 0.8em; color: rgb(59,89,152);"> 
+Comment 10
+		</p>		
+		</div>
+		
+		</div>
+		
+		<!-- THIRD CONTENT -->
+		<div id="user-box-content-container">
+
+		<div id="user-box-content-icon">
+			<img src="<?php echo base_url(); ?>assets/images/user1.png" />
+		</div>
+		<div id="user-box-content-text">		
+		<font style="font-family: Gisha; font-size: 1em; color: rgb(59,89,152)"><strong>
+TA Topik Mikroelektronika</strong>
+		<font style="font-family: Gisha; font-size: 0.9em; color: rgb(131,130,129)">
+- 1 November 2012
+		</font>
+		<p style="font-family: Gisha; font-size: 1em; color: black;"> 
+Examine the history of MIT through the lens of science and technology. Beginning with its founding in 1861, the lecture videos cover topics like William Barton Rogers,educational philosophy, and the Institute’s relationship with Boston. 
+		</p>
+		<p style="font-family: Gisha; font-size: 0.8em; color: rgb(59,89,152);"> 
+Comment 120
+		</p>
+		</div>
+		
+		</div>	
+
+		<!-- FORTH CONTENT -->
+		<div id="user-box-content-container" style="background-color: rgb(222,222,221)">
+
+		<div id="user-box-content-icon">
+			<img src="<?php echo base_url(); ?>assets/images/user2.png" />
+		</div>
+		<div id="user-box-content-text">		
+		<font style="font-family: Gisha; font-size: 1em; color: rgb(59,89,152)"><strong>
+Materi Kuliah Manfar</strong>
+		<font style="font-family: Gisha; font-size: 0.9em; color: rgb(131,130,129)">
+- 1 November 2012
+		</font>
+		<p style="font-family: Gisha; font-size: 1em; color: black;"> 
+Whether you're in college, graduate school, high school or taking some night classes after work, there are some important ways to stay organized to ensure success in school. And if you're finding that schoolwork is taking up all of your time, MainStreet has some suggestions on how to balance schoolwork with everything else. 
+		</p>
+		<p style="font-family: Gisha; font-size: 0.8em; color: rgb(59,89,152);"> 
+Comment 10
+		</p>		
+		</div>
+		
+		</div>		
+			
+			
+		<!-- FIFTH CONTENT -->
+		<div id="user-box-content-container">
+
+		<div id="user-box-content-icon">
+			<img src="<?php echo base_url(); ?>assets/images/user1.png" />
+		</div>
+		<div id="user-box-content-text">		
+		<font style="font-family: Gisha; font-size: 1em; color: rgb(59,89,152)"><strong>
+TA Topik Mikroelektronika</strong>
+		<font style="font-family: Gisha; font-size: 0.9em; color: rgb(131,130,129)">
+- 1 November 2012
+		</font>
+		<p style="font-family: Gisha; font-size: 1em; color: black;"> 
+Examine the history of MIT through the lens of science and technology. Beginning with its founding in 1861, the lecture videos cover topics like William Barton Rogers,educational philosophy, and the Institute’s relationship with Boston. 
+		</p>
+		<p style="font-family: Gisha; font-size: 0.8em; color: rgb(59,89,152);"> 
+Comment 120
+		</p>
+		</div>
+		
+		</div>	
+		
+	</div>
+</div>
+
+<div style="height: 30px; text-align: center; padding: 50px 0px 30px 0px;">
+	<font style="font-family: Gisha; font-size: 1.5em;">Random Top Rated Topics</font>
+</div>
+
+<div id="category-box-content">
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic1.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic2.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic3.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic4.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic5.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic6.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic7.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic8.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic9.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic10.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic1.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic2.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic3.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic4.png" /></a></div>
+	<div id="category-box-content-icon"><a href="<?php echo site_url('topics') ?>"><img src="<?php echo base_url(); ?>assets/images/topic5.png" /></a></div>
+</div>	
